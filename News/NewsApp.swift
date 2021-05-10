68f20ff70207311fc9e79a48cdff638a56beb201
orangeboy <chenenhan@qq.com>
2021-05-10 10:15:48 +0800

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by orangeboy on 2021/5/10.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
